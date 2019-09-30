61779c72b0f07a84bbc4b6ecb3470ea5dd6fa933
Ning Wang <unidentifiedme26@gmail.com>
2019-09-30 21:32:53 +0800

    add solutions

--- go/143.reorder-list.go
+++ go/143.reorder-list.go
@@ -11,6 +11,28 @@
  * }
  */
 func reorderList(head *ListNode)  {
-    
+	if head == nil || head.Next == nil {
+		return
+	}
+
+	var nodes []*ListNode
+	for cur := head; cur != nil; cur = cur.Next {
+		nodes = append(nodes, cur)
+	}
+
+	index := len(nodes) - 1
+	i, j := 0, index
+	for i < j {
+		if j < index {
+			nodes[j+1].Next = nodes[i]
+		}
+		nodes[i].Next = nodes[j]
+		i++
+		j--
+	}
+	if i == j {
+		nodes[j+1].Next = nodes[i]
+	}
+	nodes[i].Next = nil
 }
 
