5514dc6d044517750590d85682b809fa8d19c50a
Ning Wang <unidentifiedme26@gmail.com>
2019-11-02 22:03:39 +0800

    add solutions

---
+++ go/152.maximum-product-subarray.go
@@ -0,0 +1,33 @@
+import "math"
+
+/*
+ * @lc app=leetcode id=152 lang=golang
+ *
+ * [152] Maximum Product Subarray
+ */
+
+// @lc code=start
+func maxProduct(nums []int) int {
+	var product int
+	max := math.MinInt32
+	products := make([]int, len(nums))
+
+	for i := 0; i < len(nums); i++ {
+		for j := i; j < len(nums); j++ {
+			if j == i {
+				product = nums[i]
+			} else {
+				product = products[j-1] * nums[j]
+			}
+			products[j] = product
+			if product > max {
+				max = product
+			}
+		}
+	}
+
+	return max
+}
+
+// @lc code=end
+
