32a6f8cffcb3130f66141ec333fbcfdc15298a7f
Ning Wang <unidentifiedme26@gmail.com>
2019-11-07 23:30:10 +0800

    add solutions

---
+++ go/95.unique-binary-search-trees-ii.go
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=95 lang=golang
+ *
+ * [95] Unique Binary Search Trees II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func generateTrees(n int) []*TreeNode {
+	return generate(1, n)
+}
+
+func generate(begin, end int) []*TreeNode {
+	if begin == end {
+		return []*TreeNode{&TreeNode{Val: begin}}
+	}
+	var ret []*TreeNode
+	for i := begin; i <= end; i++ {
+		var lefts, rights []*TreeNode
+		if i == begin {
+			lefts = []*TreeNode{nil}
+			rights = generate(i+1, end)
+		} else if i == end {
+			lefts = generate(begin, i-1)
+			rights = []*TreeNode{nil}
+		} else {
+			lefts = generate(begin, i-1)
+			rights = generate(i+1, end)
+		}
+		for _, left := range lefts {
+			for _, right := range rights {
+				ret = append(ret, &TreeNode{Val: i, Left: left, Right: right})
+			}
+		}
+	}
+	return ret
+}
+
+// @lc code=end
+
