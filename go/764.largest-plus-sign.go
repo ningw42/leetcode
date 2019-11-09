95524ffda8570d02a2709942c15a4f908c812eae
Ning Wang <unidentifiedme26@gmail.com>
2019-11-10 01:09:41 +0800

    add solutions

---
+++ go/764.largest-plus-sign.go
@@ -0,0 +1,74 @@
+import (
+	"math"
+)
+
+/*
+ * @lc app=leetcode id=764 lang=golang
+ *
+ * [764] Largest Plus Sign
+ */
+
+// @lc code=start
+func orderOfLargestPlusSign(N int, mines [][]int) int {
+	mat := buildMatrix(N, mines)
+	max := math.MinInt32
+	for i := 0; i < N; i++ {
+		for j := 0; j < N; j++ {
+			order := findOrder(mat, i, j)
+			// fmt.Println(i, j, order)
+			if order > max {
+				max = order
+			}
+		}
+	}
+
+	return max
+}
+
+func findOrder(mat [][]bool, i, j int) int {
+	if mat[i][j] {
+		return 0
+	}
+
+	N := len(mat)
+	diff := 1
+	var x, y int
+loop:
+	for {
+		coods := [][]int{
+			[]int{i + diff, j},
+			[]int{i - diff, j},
+			[]int{i, j + diff},
+			[]int{i, j - diff},
+		}
+		for _, cood := range coods {
+			x = cood[0]
+			y = cood[1]
+			if 0 <= x && x < N && 0 <= y && y < N {
+				if mat[x][y] {
+					break loop
+				}
+			} else {
+				break loop
+			}
+		}
+		diff++
+	}
+
+	return diff
+}
+
+func buildMatrix(N int, mines [][]int) [][]bool {
+	var mat [][]bool
+	for i := 0; i < N; i++ {
+		mat = append(mat, make([]bool, N))
+	}
+	for _, cood := range mines {
+		mat[cood[0]][cood[1]] = true
+	}
+
+	return mat
+}
+
+// @lc code=end
+
---
+++ go/787.cheapest-flights-within-k-stops.go
@@ -0,0 +1,128 @@
+import "math"
+
+/*
+ * @lc app=leetcode id=787 lang=golang
+ *
+ * [787] Cheapest Flights Within K Stops
+ */
+
+// @lc code=start
+
+type Graph struct {
+	Edges map[int]map[int]int
+}
+
+func findCheapestPrice(n int, flights [][]int, src int, dst int, K int) int {
+	g := buildGraph(n, flights)
+	K = K + 1 // covert from # nodes to # edges
+
+	// return DFS(g, src, dst, K)
+	return DP(g, src, dst, n, K)
+}
+
+func buildGraph(n int, flights [][]int) *Graph {
+	edges := make(map[int]map[int]int)
+
+	for _, flight := range flights {
+		from := flight[0]
+		to := flight[1]
+		weight := flight[2]
+		if _, e1 := edges[from]; e1 {
+			edges[from][to] = weight
+		} else {
+			edges[from] = map[int]int{to: weight}
+		}
+	}
+
+	return &Graph{Edges: edges}
+}
+
+// F(i, j, k) = F(i, a, k-1) + F(a, j, 1)
+func DP(g *Graph, src, dst, n, k int) int {
+	var results [][][]int
+	for i := 0; i <= k; i++ {
+		results = append(results, makeEmpty(n))
+	}
+	// 0
+	for i := 0; i < n; i++ {
+		results[0][i][i] = 0
+	}
+	// 1
+	for from, tos := range g.Edges {
+		for to, weight := range tos {
+			results[1][from][to] = weight
+		}
+	}
+
+	for stop := 2; stop <= k; stop++ {
+		for from, tos := range g.Edges {
+			for to, weight := range tos {
+				for final, restWeight := range results[stop-1][to] {
+					if restWeight >= 0 {
+						newResult := weight + restWeight
+						if existingResult := results[stop][from][final]; existingResult < 0 {
+							results[stop][from][final] = newResult
+						} else {
+							if newResult < existingResult {
+								results[stop][from][final] = newResult
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	min := math.MaxInt32
+	for i := 0; i <= k; i++ {
+		if weight := results[i][src][dst]; weight >= 0 && weight < min {
+			min = weight
+		}
+	}
+	if min == math.MaxInt32 {
+		min = -1
+	}
+
+	return min
+}
+func makeEmpty(n int) [][]int {
+	var ret [][]int
+	for i := 0; i < n; i++ {
+		ret = append(ret, make([]int, n))
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			ret[i][j] = -1
+		}
+	}
+
+	return ret
+}
+
+// wont AC, time limit exceeded
+func DFS(g *Graph, src, dst, k int) int {
+	if src == dst {
+		return 0
+	} else {
+		if k == 0 {
+			return -1
+		}
+	}
+	min := math.MaxInt32
+	for next, weight := range g.Edges[src] {
+		if weight > 0 {
+			w := DFS(g, next, dst, k-1)
+			if w >= 0 && w+weight < min {
+				min = w + weight
+			}
+		}
+	}
+	if min == math.MaxInt32 {
+		min = -1
+	}
+
+	return min
+}
+
+// @lc code=end
+
