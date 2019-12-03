e11ac929327eedc188909897e572d7457f63a9b8
Ning Wang <unidentifiedme26@gmail.com>
2019-12-04 01:23:25 +0800

    add solutions

---
+++ go/48.rotate-image.go
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=48 lang=golang
+ *
+ * [48] Rotate Image
+ */
+
+// @lc code=start
+func rotate(matrix [][]int) {
+	var stash int
+	for i := 0; i < len(matrix) / 2; i++ {
+		// beware of the condition 'j > i'
+		for j := len(matrix)-1-i; j > i; j-- {
+			// stash top
+			stash = matrix[i][j]
+			// rotate left to top
+			matrix[i][j] = matrix[len(matrix)-1-j][i]
+			// rotate bottom to left
+			matrix[len(matrix)-1-j][i] = matrix[len(matrix)-1-i][len(matrix)-1-j]
+			// rotate right to bottom
+			matrix[len(matrix)-1-i][len(matrix)-1-j] = matrix[j][len(matrix)-1-i]
+			// rotate top to right
+			matrix[j][len(matrix)-1-i] = stash
+		}
+	}
+}
+
+func naive(matrix [][]int) {
+	for i := 0; i < len(matrix) / 2; i++ {
+		// stash top
+		tmp := make([]int, len(matrix) - 2 * i)
+		for j := len(matrix)-1-i; j >= i; j-- {
+			tmp[j-i] = matrix[i][j]
+		}
+		// rotate left to top
+		for j := len(matrix)-1-i; j >= i; j-- {
+			matrix[i][j] = matrix[len(matrix)-1-j][i]
+		}
+		// rotate bottom to left
+		for j := len(matrix)-1-i; j >= i; j-- {
+			matrix[len(matrix)-1-j][i] = matrix[len(matrix)-1-i][len(matrix)-1-j]
+		}
+		// rotate right to bottom
+		for j := len(matrix)-1-i; j >= i; j-- {
+			matrix[len(matrix)-1-i][len(matrix)-1-j] = matrix[j][len(matrix)-1-i]
+		}
+		// rotate top to right
+		for j := len(matrix)-1-i; j >= i; j-- {
+			matrix[j][len(matrix)-1-i] = tmp[j-i]
+		}
+	}
+}
+
+// @lc code=end
+
