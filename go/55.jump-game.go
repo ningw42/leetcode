3c6d6cbbefde68dc9aba3bb00c191231e35bebdf
Ning Wang <unidentifiedme26@gmail.com>
2019-12-09 00:52:42 +0800

    add solutions

---
+++ go/55.jump-game.go
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=55 lang=golang
+ *
+ * [55] Jump Game
+ */
+
+// @lc code=start
+func canJump(nums []int) bool {
+	steps := make([]int, len(nums))
+	for i := len(nums) - 2; i >= 0; i-- {
+		step := math.MaxInt32
+		for j := nums[i]; j >= 1; j-- {
+			if i+j >= len(nums) {
+				step = 0
+				break
+			}
+			if newStep := steps[i+j]; newStep < step {
+				step = newStep
+			}
+		}
+		steps[i] = step + 1
+	}
+	return steps[0] != math.MaxInt32 + 1
+}
+// @lc code=end
+
