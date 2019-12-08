a108109e0950905ee43acf2fdafbdc37c7a8f63c
Ning Wang <unidentifiedme26@gmail.com>
2019-12-09 00:32:53 +0800

    add solutions

---
+++ go/50.pow-x-n.go
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=50 lang=golang
+ *
+ * [50] Pow(x, n)
+ */
+
+// @lc code=start
+func myPow(x float64, n int) float64 {
+	var minusN, minusX bool
+	if x < 0 {
+		x = -x
+		minusX = true
+	}
+	if n < 0 {
+		n = -n
+		minusN = true
+	}
+
+	pow := naivePow(x, n)
+
+	var result float64
+	if minusN {
+		result = 1.0 / pow
+	} else {
+		result = pow
+	}
+	if minusX && n % 2 == 1 {
+		result = -result
+	}
+	return result
+}
+
+func naivePow(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	} else {
+		half := naivePow(x, n / 2)
+		if result := half * half; n % 2 == 0 {
+			return result
+		} else {
+			return result * x
+		}
+	}
+}
+// @lc code=end
+
