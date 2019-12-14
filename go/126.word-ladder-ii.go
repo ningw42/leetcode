e0f760376fde118f722e85bac5f7ed6f068dc21e
Ning Wang <unidentifiedme26@gmail.com>
2019-12-14 12:30:17 +0800

    add solutions

---
+++ go/126.word-ladder-ii.go
@@ -0,0 +1,142 @@
+/*
+ * @lc app=leetcode id=126 lang=golang
+ *
+ * [126] Word Ladder II
+ */
+
+// @lc code=start
+
+// naive DFS/backtracing wont work, time limit exceed
+// convert to graph and find shortest path
+func findLadders(beginWord string, endWord string, wordList []string) [][]string {
+	// misc preparation
+	var numberOfVertex, begin, end int
+	var beginInList, endInList bool
+	for i := 0; i < len(wordList); i++ {
+		if beginWord == wordList[i] {
+			begin = i
+			beginInList = true 
+		}
+		if endWord == wordList[i] {
+			end = i
+			endInList = true
+		}
+	}
+	if !endInList {
+		return nil
+	}
+	if beginInList {
+		numberOfVertex = len(wordList)
+	} else {
+		begin = len(wordList)
+		numberOfVertex = len(wordList)+1
+	}
+	// build graph
+	var g [][]bool 
+	for i := 0; i < numberOfVertex; i++ {
+		g = append(g, make([]bool, numberOfVertex))
+	}
+	for i := 0; i < len(wordList); i++ {
+		for j := i+1; j < len(wordList); j++ {
+			if diff(wordList[i], wordList[j]) == 1 {
+				g[i][j] = true
+				g[j][i] = true
+			}
+		}
+	}
+	if !beginInList {
+		for i := 0; i < len(wordList); i++ {
+			if diff(beginWord, wordList[i]) == 1 {
+				g[begin][i] = true
+				g[i][begin] = true
+			}
+		}
+	}
+
+	// shortest path from begin to end
+	// BFS
+	queue := []int{begin}
+	// seen holds if the vertex has been seen
+	// seen[i] holds the shortest pathes from begin to i(share the same length)
+	seen := make([][][]int, numberOfVertex)
+	seen[begin] = [][]int{[]int{begin}}
+	BFS:
+	for len(queue) > 0 {
+		from := queue[0]
+		queue = queue[1:]
+		for to, edgeExists := range g[from] {
+			if edgeExists {
+				// remove edge from graph
+				g[from][to] = false
+				g[to][from] = false
+
+				if len(seen[to]) == 0 {
+					// if vertex has not been seen yet
+					// add to queue and mark as seen
+					queue = append(queue, to)
+					seen[to] = append(seen[to], appendToAll(seen[from], to)...)
+				} else {
+					// if vertex has been seen
+					// if the new path share the same length, append 
+					// if the new path length is greater than existing path length, ignore, if the 'to' vertex is our target, stop BFS
+					// it is impossible that the length of new path is smaller than existing path length, since we are doing BFS
+					if existingPathLength := len(seen[to][0]); existingPathLength == len(seen[from][0])+1 {
+						seen[to] = append(seen[to], appendToAll(seen[from], to)...)
+					} else if existingPathLength < len(seen[from][0])+1 {
+						if to == end {
+							break BFS
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// translate result
+	pathes := seen[end]
+	if len(pathes) == 0 {
+		return nil
+	} else {
+		var sPathes [][]string
+		for _, path := range pathes {
+			var sPath []string
+			for _, stop := range path {
+				if stop == begin {
+					sPath = append(sPath, beginWord)
+				} else {
+					sPath = append(sPath, wordList[stop])
+				}
+			}
+			sPathes = append(sPathes, sPath)
+		}
+		return sPathes
+	}
+}
+
+func diff(a, b string) int {
+	var count int
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			count++
+		}
+	}
+	return count
+}
+
+func appendToAll(s [][]int, t int) [][]int {
+	var ret [][]int
+	for _, p := range s {
+		ret = append(ret, append(makeSliceCopy(p), t))
+	}
+	return ret
+}
+
+func makeSliceCopy(s []int) []int {
+	copy := make([]int, len(s))
+	for i, e := range s {
+		copy[i] = e
+	}
+	return copy
+}
+// @lc code=end
+
