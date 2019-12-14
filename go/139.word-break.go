37e3da3cd3c4995b2a51fb1375f3fd9d8c2e438e
Ning Wang <unidentifiedme26@gmail.com>
2019-12-14 22:43:10 +0800

    add solutions

---
+++ go/139.word-break.go
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=139 lang=golang
+ *
+ * [139] Word Break
+ */
+
+// @lc code=start
+func wordBreak(s string, wordDict []string) bool {
+	dict := make(map[string]bool)
+	for _, word := range wordDict {
+		dict[word] = true
+	}
+
+	result := make([]bool, len(s))
+
+	for i := 0; i < len(s); i++ {
+		for j := -1; j < i; j++ {
+			if _, exists := dict[s[j+1:i+1]]; exists && (j == -1 || result[j]) {
+				result[i] = true
+				break
+			}
+		}
+	}
+
+	return result[len(s)-1]
+}
+// @lc code=end
+
---
+++ go/140.word-break-ii.go
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode id=140 lang=golang
+ *
+ * [140] Word Break II
+ */
+
+// @lc code=start
+
+// naive DFS wont work, time limit exceeded
+// DFS with memorization
+func wordBreak(s string, wordDict []string) []string {
+	return DFS(s, wordDict, map[string][]string{"":[]string{""}})
+}
+
+func DFS(s string, dict []string, results map[string][]string) []string {
+	if r, exists := results[s]; exists {
+		return r
+	}
+	var result []string
+	for _, word := range dict {
+		if strings.HasPrefix(s, word) {
+			for _, sentence := range DFS(s[len(word):], dict, results) {
+				result = append(result, strings.Trim(word+" "+sentence, " "))
+			}
+		}
+	}
+	results[s] = result
+	return result
+}
+
+// AC with bug
+// memory limit exceeded on following case
+// "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+// ["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
+func wordBreakDP(s string, wordDict []string) []string {
+	dict := make(map[string]bool)
+	for _, word := range wordDict {
+		dict[word] = true
+	}
+	if !canBreakDP(s, dict) {
+		return nil
+	}
+
+	result := make([][]string, len(s))
+	for i := 0; i < len(s); i++ {
+		if _, exists := dict[s[:i+1]]; exists {
+			result[i] = []string{s[:i+1]}
+		}
+		for j := 0; j < i; j++ {
+			if _, exists := dict[s[j+1:i+1]]; exists && len(result[j]) > 0 {
+				for _, sentence := range result[j] {
+					result[i] = append(result[i], sentence+" "+s[j+1:i+1])
+				}
+			}
+		}
+	}
+
+	return result[len(s)-1]
+}
+
+func canBreakDP(s string, dict map[string]bool) bool {
+	result := make([]bool, len(s))
+
+	for i := 0; i < len(s); i++ {
+		for j := -1; j < i; j++ {
+			if _, exists := dict[s[j+1:i+1]]; exists && (j == -1 || result[j]) {
+				result[i] = true
+				break
+			}
+		}
+	}
+
+	return result[len(s)-1]
+}
+// @lc code=end
+
