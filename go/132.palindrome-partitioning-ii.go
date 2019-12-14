939f1449a295e83076ab7e454355596aceac24a6
Ning Wang <unidentifiedme26@gmail.com>
2019-12-14 23:51:23 +0800

    add solutions

---
+++ go/132.palindrome-partitioning-ii.go
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=132 lang=golang
+ *
+ * [132] Palindrome Partitioning II
+ */
+
+// @lc code=start
+
+// DP
+// minCut(i) = min(minCut(j)+1) for 0 <= j < i and s[j:i] is palindrome
+func minCut(s string) int {
+	minCuts := make([]int, len(s)+1)
+	for i := 0; i <= len(s); i++ {
+		minCuts[i] = math.MaxInt32
+	}
+	minCuts[0] = -1
+	for i := 1; i <= len(s); i++ {
+		min := math.MaxInt32
+		for j := 0; j < i; j++ {
+			if minCuts[j] != math.MaxInt32 && isPalindrome(s[j:i]) {
+				if cut := minCuts[j] + 1; cut < min {
+					min = cut
+				}
+			}
+		}
+		minCuts[i] = min
+	}
+	return minCuts[len(s)]
+}
+
+func isPalindrome(s string) bool {
+	for i := 0; i < len(s) / 2; i++ {
+		if s[i] != s[len(s)-1-i] {
+			return false
+		}
+	}
+	return true
+}
+// @lc code=end
+
