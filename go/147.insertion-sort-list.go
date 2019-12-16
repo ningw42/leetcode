c9ed24922b21b24a882426c247346fb1b6169cc3
Ning Wang <unidentifiedme26@gmail.com>
2019-12-16 23:51:14 +0800

    add solutions

---
+++ go/147.insertion-sort-list.go
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=147 lang=golang
+ *
+ * [147] Insertion Sort List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func insertionSortList(head *ListNode) *ListNode {
+	var current, prev, next *ListNode
+	dummy := &ListNode{Next:nil}
+	unsorted := head
+	for unsorted != nil {
+		prev, current = dummy, dummy.Next
+		for current != nil && current.Val < unsorted.Val {
+			prev = current
+			current = current.Next
+		}
+		prev.Next = unsorted
+		next = unsorted.Next
+		unsorted.Next = current
+		unsorted = next
+	}
+	return dummy.Next
+}
+// @lc code=end
+
---
+++ go/149.max-points-on-a-line.go
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode id=149 lang=golang
+ *
+ * [149] Max Points on a Line
+ */
+
+// @lc code=start
+func maxPoints(points [][]int) int {
+	m := make(map[string]int)
+	for _, point := range points {
+		m[strconv.Itoa(point[0])+","+strconv.Itoa(point[1])]++
+	}
+
+	var filteredPoints [][]int
+	var count []int
+	for point, c := range m {
+		count = append(count, c)
+		cood := strings.Split(point, ",")
+		x, _ := strconv.Atoi(cood[0])
+		y, _ := strconv.Atoi(cood[1])
+		filteredPoints = append(filteredPoints, []int{x, y})
+	}
+
+	return maxPointsHelper(filteredPoints, count)
+}
+
+func maxPointsHelper(points [][]int, count []int) int {
+	if len(points) <= 2 {
+		var sum int
+		for _, c := range count {
+			sum += c
+		}
+		return sum
+	}
+	var checked [][]bool
+	for i := 0; i < len(points); i++ {
+		checked = append(checked, make([]bool, len(points)))
+	}
+	max := 0
+	for i := 0; i < len(points); i++ {
+		for j := i+1; j < len(points); j++ {
+			if !checked[i][j] {
+				numberOfPoints := count[i] + count[j]
+				for k := 0; k < len(points); k++ {
+					if k == i || k == j {
+						continue
+					}
+					var onSameLine bool
+					if points[i][0] == points[j][0] {
+						onSameLine = points[k][0] == points[i][0]
+					} else if points[i][1] == points[j][1] {
+						onSameLine = points[k][1] == points[i][1]
+					} else {
+						// amplify the K will increase the precision
+						K := 100 * float64(points[j][1] - points[i][1]) / float64(points[j][0] - points[i][0])
+						onSameLine = 100 * float64(points[k][1] - points[i][1]) / float64(points[k][0] - points[i][0]) == K
+					}
+					if onSameLine {
+						numberOfPoints += count[k]
+						checked[i][k] = true
+						checked[k][i] = true
+						checked[j][k] = true
+						checked[k][j] = true
+					}
+				}
+				if numberOfPoints > max {
+					max = numberOfPoints
+				}
+			}
+		}
+	}
+	return max
+}
+// @lc code=end
+
