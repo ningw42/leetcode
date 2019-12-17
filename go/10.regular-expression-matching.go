bac7f1c752c32ba37a27c6e4eedd106f4915983f
Ning Wang <unidentifiedme26@gmail.com>
2019-12-18 00:00:15 +0800

    add solutions

---
+++ go/10.regular-expression-matching.go
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode id=10 lang=golang
+ *
+ * [10] Regular Expression Matching
+ */
+
+// @lc code=start
+func isMatch(s string, p string) bool {
+	return match(s, p)
+}
+
+func match(s, p string) bool {
+	if s == "" && p == "" {
+		return true
+	} else if s != "" && p == "" {
+		return false
+	} else if s == "" && p != "" {
+		// s is empty, all the rest p should be consist of wildcard token to match
+		token := nextToken(p)
+		if len(token) == 2 {
+			return match(s, p[2:])
+		} else {
+			return false
+		}
+	} else {
+		token := nextToken(p)
+		if len(token) == 1 {
+			// token with length 1
+			if token == "." {
+				// single wildcard token
+				return match(s[1:], p[1:])
+			} else {
+				// single alphabet token
+				return s[0] == token[0] && match(s[1:], p[1:])
+			}
+		} else {
+			// token with length 2, "a*", ".*" ish
+			if token[0] == '.' {
+				// wildcard kleene star token
+				for i := 0; i <= len(s); i++ {
+					if match(s[i:], p[2:]) {
+						return true
+					}
+				}
+				return false
+			} else {
+				// alphabet kleene star token
+				for i := 0; i <= len(s) && (i == 0 || s[i-1] == token[0]); i++ {
+					if match(s[i:], p[2:]) {
+						return true
+					}
+				}
+				return false
+			}
+		}
+	}
+}
+
+func nextToken(s string) string {
+	if len(s) > 1 {
+		if s[1] == '*' {
+			return s[:2]
+		} else {
+			return s[:1]
+		}
+	} else {
+		return s[:1]
+	}
+}
+// @lc code=end
+
