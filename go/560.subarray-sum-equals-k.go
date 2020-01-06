3a1b5d7d4d00b815f010b694a1185f2d0c3414e0
Ning Wang <unidentifiedme26@gmail.com>
2020-01-06 22:32:13 +0800

    add solutions

---
+++ go/560.subarray-sum-equals-k.go
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=560 lang=golang
+ *
+ * [560] Subarray Sum Equals K
+ */
+
+// @lc code=start
+func subarraySum(nums []int, k int) int {
+    return HashMap(nums, k)
+}
+
+// TC O(n)
+// SC O(n)
+func HashMap(nums []int, k int) int {
+    // map from 'sum' to '# of occurance of sum'
+    sums := map[int]int{
+        0: 1,
+    }
+   
+    var sum, count int
+    for _, num := range nums {
+        sum += num
+        if numberOfBegin, exists := sums[sum-k]; exists {
+            count += numberOfBegin
+        }
+        sums[sum]++
+    }
+    return count
+}
+
+// TC O(n^2)
+// SC O(n)
+// SC can be optimized to O(1)
+func NaiveDP(nums []int, k int) int {
+    count := 0
+    sums := make([]int, len(nums))
+    for i, sum := range nums {
+        sums[i] = sum
+        if sum == k {
+            count++
+        }
+    }
+    for length := 2; length <= len(nums); length++ {
+        for i := 0; i + length <= len(nums); i++ {
+            sums[i] = sums[i] + nums[i+length-1]
+            if sums[i] == k {
+                count++
+            }
+        }
+    }
+    return count
+}
+// @lc code=end
+
