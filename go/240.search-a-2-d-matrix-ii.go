e2f7a4041b15f4c75667735cb2c9f6387b1b3df4
Ning Wang <unidentifiedme26@gmail.com>
2020-01-14 23:14:08 +0800

    add solutions

---
+++ go/240.search-a-2-d-matrix-ii.go
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=240 lang=golang
+ *
+ * [240] Search a 2D Matrix II
+ */
+
+// @lc code=start
+// tilt the matrix, it looks like a BST
+// search from the top right corner
+// O(m+n)
+func searchMatrix(matrix [][]int, target int) bool {
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return false
+	}
+	row := 0
+	col := len(matrix[0]) - 1
+	for row < len(matrix) && col >= 0 {
+		if value := matrix[row][col]; target > value {
+			row++
+		} else if target < value {
+			col--
+		} else {
+			return true
+		}
+	}
+	return false
+}
+// @lc code=end
+
