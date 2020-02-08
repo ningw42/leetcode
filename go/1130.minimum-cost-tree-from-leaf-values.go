c3db53de40a100b6111d589072237444c0487466
Ning Wang <unidentifiedme26@gmail.com>
2020-02-08 22:55:27 +0800

    add solutions

---
+++ go/1130.minimum-cost-tree-from-leaf-values.go
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode id=1130 lang=golang
+ *
+ * [1130] Minimum Cost Tree From Leaf Values
+ */
+
+// @lc code=start
+
+// S(i, j) = min(S(i, k) + S(k+1, j) + MAX(i, k) * MAX(k+1, j)) for every k with i <= k < j
+// MAX(i, j) = max(arr[i]...arr[j])
+func mctFromLeafValues(arr []int) int {
+	// DP to calculate MAX(i,j)
+	maxs := make([][]int, len(arr))
+	for i := 0; i < len(arr); i++ {
+		maxs[i] = make([]int, len(arr))
+	}
+	for i := 0; i < len(arr); i++ {
+		for j := i; j < len(arr); j++ {
+			if j == i {
+				maxs[i][j] = arr[i]
+			} else {
+				maxs[i][j] = max(maxs[i][j-1], arr[j])
+			}
+		}
+	}
+
+	// DP to calculate S(i, j)
+	mcts := make([][]int, len(arr))
+	for i := 0; i < len(arr); i++ {
+		mcts[i] = make([]int, len(arr))
+	}
+	for step := 1; step < len(arr); step++ {
+		for i := 0; i < len(arr); i++ {
+			j := i + step
+			if j >= len(arr) {
+				continue
+			}
+			result := math.MaxInt32
+			for k := i; k < j; k++ {
+				result = min(
+					result,
+					mcts[i][k] + mcts[k+1][j] + maxs[i][k] * maxs[k+1][j],
+				)
+			}
+			mcts[i][j] = result
+		}
+	}
+
+	return mcts[0][len(arr)-1]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a 
+	} else {
+		return b
+	}
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+// @lc code=end
+
