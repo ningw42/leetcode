6600435ddb0f64402577aea7730862ff27eebafb
Ning Wang <unidentifiedme26@gmail.com>
2020-03-03 23:54:36 +0800

    add solutions

--- go/84.largest-rectangle-in-histogram.go
+++ go/84.largest-rectangle-in-histogram.go
@@ -4,11 +4,46 @@
  * [84] Largest Rectangle in Histogram
  */
 func largestRectangleArea(heights []int) int {
-	return DP(heights)
+	return MonotoneStack(heights)
 }
 
-func DP(heights []int) int {
-	// TODO
+func MonotoneStack(heights []int) int {
+	left := make([]int, len(heights))
+    right := make([]int, len(heights))
+    var stack []int
+    for i, h := range heights {
+        for len(stack) != 0 && heights[stack[len(stack)-1]] >= h {
+            stack = stack[:len(stack)-1]
+        }
+        if len(stack) == 0 {
+            left[i] = -1
+        } else {
+            left[i] = stack[len(stack)-1]
+        }
+        stack = append(stack, i)
+    }
+    stack = nil
+    for i := len(heights)-1; i >= 0; i-- {
+        h := heights[i]
+        for len(stack) != 0 && heights[stack[len(stack)-1]] >= h {
+            stack = stack[:len(stack)-1]
+        }
+        if len(stack) == 0 {
+            right[i] = len(heights)
+        } else {
+            right[i] = stack[len(stack)-1]
+        }
+        stack = append(stack, i)
+    }
+ 
+    max := 0
+    for i, h := range heights {
+        if area := h * (right[i]-left[i]-1); area > max {
+            max = area
+        }
+    }
+    
+    return max
 }
 
 func BruteForce(heights []int) int {
