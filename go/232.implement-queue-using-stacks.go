f5ddad706d37f4af0a284ebd6795c74e997be260
Ning Wang <unidentifiedme26@gmail.com>
2020-03-08 23:58:34 +0800

    add solutions

---
+++ go/232.implement-queue-using-stacks.go
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode id=232 lang=golang
+ *
+ * [232] Implement Queue using Stacks
+ */
+
+// @lc code=start
+type MyQueue struct {
+    in []int
+    out []int
+}
+
+
+/** Initialize your data structure here. */
+func Constructor() MyQueue {
+    return MyQueue{}
+}
+
+func (mq *MyQueue) dumpFromInToOut() {
+    for len(mq.in) > 0 {
+        item := mq.in[len(mq.in)-1]
+        mq.in = mq.in[:len(mq.in)-1]
+        mq.out = append(mq.out, item)
+    }
+}
+
+/** Push element x to the back of queue. */
+func (this *MyQueue) Push(x int)  {
+    this.in = append(this.in, x)
+}
+
+
+/** Removes the element from in front of queue and returns that element. */
+func (this *MyQueue) Pop() int {
+    if len(this.out) == 0 {
+        this.dumpFromInToOut()
+    }
+    item := this.out[len(this.out)-1]
+    this.out = this.out[:len(this.out)-1]
+    return item
+}
+
+
+/** Get the front element. */
+func (this *MyQueue) Peek() int {
+    if len(this.out) == 0 {
+        this.dumpFromInToOut()
+    }
+    return this.out[len(this.out)-1]
+}
+
+
+/** Returns whether the queue is empty. */
+func (this *MyQueue) Empty() bool {
+    return len(this.in) + len(this.out) == 0
+}
+
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Peek();
+ * param_4 := obj.Empty();
+ */
+// @lc code=end
+
---
+++ go/496.next-greater-element-i.go
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=496 lang=golang
+ *
+ * [496] Next Greater Element I
+ */
+
+// @lc code=start
+func nextGreaterElement(nums1 []int, nums2 []int) []int {
+    // monotone stack
+    var stack []int
+    pos := make(map[int]int)
+    for i := len(nums2) - 1; i >= 0; i-- {
+        if len(stack) == 0 {
+            pos[nums2[i]] = -1
+            stack = append(stack, nums2[i])
+        } else {
+            for len(stack) > 0 && stack[len(stack)-1] <= nums2[i] {
+                stack = stack[:len(stack)-1]
+            }
+            if len(stack) == 0 {
+                pos[nums2[i]] = -1
+            } else {
+                pos[nums2[i]] = stack[len(stack)-1]
+            }
+            stack = append(stack, nums2[i])
+        }
+    }
+    var ret []int
+    for _, num := range nums1 {
+        ret = append(ret, pos[num])
+    }
+    return ret
+}
+// @lc code=end
+
